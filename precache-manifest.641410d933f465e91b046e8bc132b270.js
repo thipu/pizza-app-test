f8b1991432b5159d982f02361efe9627894f329d
Bhajanthri Thippamma <bhajanthri.t@sonata-software.com>
2018-12-11 14:47:15 +0530

    Updates

---
+++ precache-manifest.641410d933f465e91b046e8bc132b270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thipu/pizza-app-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "874c84fb4d36bb79797f",
+    "url": "/thipu/pizza-app-test/static/js/runtime~main.874c84fb.js"
+  },
+  {
+    "revision": "6f5df051e4a724ae483d",
+    "url": "/thipu/pizza-app-test/static/js/main.6f5df051.chunk.js"
+  },
+  {
+    "revision": "6259bb82e1582e041744",
+    "url": "/thipu/pizza-app-test/static/js/1.6259bb82.chunk.js"
+  },
+  {
+    "revision": "6f5df051e4a724ae483d",
+    "url": "/thipu/pizza-app-test/static/css/main.e0097529.chunk.css"
+  },
+  {
+    "revision": "6259bb82e1582e041744",
+    "url": "/thipu/pizza-app-test/static/css/1.22476a3a.chunk.css"
+  },
+  {
+    "revision": "a85a519137132a5af4ee172b5f789755",
+    "url": "/thipu/pizza-app-test/index.html"
+  }
+];
